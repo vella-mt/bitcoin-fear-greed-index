f18c30098948dc2154bf42ec5fd44ba154e13f69
vella.exe <matthewvella2005@gmail.com>
2024-07-26 00:36:51 +0200

    cleared home page

--- Home.py
+++ Home.py
@@ -1,11 +1,3 @@
 import streamlit as st
 
-# Streamlit UI
-st.title('Bitcoin Trading based on Fear and Greed Index Strategy Simulator')
-
-st.markdown("### Fear and Greed Index Value Classifications:")
-st.markdown("- **Extreme Greed**: 76-100")
-st.markdown("- **Greed**: 55-75")
-st.markdown("- **Neutral**: 47-54")
-st.markdown("- **Fear**: 26-46")
-st.markdown("- **Extreme Fear**: 1-25")
+st.write("Under construction.")
