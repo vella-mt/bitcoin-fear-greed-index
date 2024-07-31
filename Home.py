cca17a5563954795c8bc15535f97c9841de45b89
vella.exe <matthewvella2005@gmail.com>
2024-07-31 20:02:28 +0200

    add to do list to home page

--- Home.py
+++ Home.py
@@ -1,3 +1,27 @@
 import streamlit as st
 
 st.write("Under construction.")
+
+st.markdown(
+    '''
+    ### *Strategy Creator* Planned features:
+    ##### Short-term 
+    - Add buy and sell thresholds as dashed lines in *Buy/Sell Signals* plot.
+    - Add chart to plot signal strength.
+    - Configure different buy and sell trade amounts and add ability to trade percentage of balance.
+    - Write the indicators which activated each buy/sell signal and display them in plots.
+    - Add Dollar Cost Average (DCA) strategy.
+    - Add chart to plot comparison with DCA and 100% buy and hold.
+    - Integrate more strategy indicators.
+    
+    ##### Medium-term
+    - Integrate various trading patterns.
+    - Integrate *volume* trading data.
+    - Integration of other coins.
+    - Add ability to backtest strategies on multiple coins.
+    - More advanced signal selection for strategy customization (AND/OR configuration instead of total signal thresholds).
+
+    ##### Long-term
+    - API integration to allow for live trading.
+    '''
+)
